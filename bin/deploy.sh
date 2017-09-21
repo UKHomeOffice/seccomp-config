b0d1bff49635655566b0b45d2c14c5bf403c0d69
awkwardben <awks.ben@gmail.com>
2017-09-21 18:14:19 +0100

    initial daemonset deployment

---
+++ bin/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+export KUBE_NAMESPACE=kube-system
+export KUBE_SERVER=${KUBE_SERVER}
+export KUBE_TOKEN=${KUBE_TOKEN}
+
+echo "--- Kube API URL: ${KUBE_SERVER}"
+echo "--- Kube namespace: ${KUBE_NAMESPACE}"
+
+kd --insecure-skip-tls-verify \
+  --timeout 10m0s \
+  -f kube/seccomp-daemonset.yaml 
